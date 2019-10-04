9f5c19263e35f975be27939f4c533c9dc00ba080
parth <parthematics@gmail.com>
2019-10-03 23:46:32 -0700

    built deck class

--- game.py
+++ game.py
@@ -1,10 +1,35 @@
 import random
+from collections import deque
 
 class Card:
-    ''' Initializing a card. Every card is defined by a specific value and suit. '''
+    ''' Initializing a playing card. Every card is defined by a specific value and suit. '''
     def __init__(self, value, suit):
         self.val = value
         self.suit = suit
 
+    ''' Magic method to print out a playing card. Called with 'print(...)'. '''
     def __repr__(self):
         return "{0} - {1}".format(self.val, self.suit)
+
+class Deck:
+    ''' Initializing a deck of cards. Note that the list comprehension will create a total of
+        52 (4 * 13) cards with two for loops. I chose to use a double ended queue (deque) to
+        represent our deck of cards because it is optimized for card removal from the front,
+        which is analogous to dealing the cards. Also, 'deque' kind of sounds like 'deck' :) '''
+    def __init__(self):
+        self.vals = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Q', 'K']
+        self.suits = ['Clubs (♣)', 'Hearts (♥)', 'Spades (♠)', 'Diamonds (♦)']
+        self.deck = deque([Card(val, suit) for val in self.vals for suit in self.suits])
+        self.size = 52
+
+    ''' Using our deque's popleft() feature to deal the top most card from our deck of cards. '''
+    def deal_card(self):
+        if self.size > 1:
+            card = self.cards.popleft()
+            self.size -= 1
+        return card
+
+    ''' Using random module's built-in shuffle method to shuffle our deck of cards. '''
+    def shuffle_deck(self):
+        if self.size > 1:
+            random.shuffle(self.deck)
